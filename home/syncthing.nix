7388a72e7946d130f0999fa72741139e9f3b3c1f
IceMoonMagic <icemoonmagic@gmail.com>
2025-10-11 20:59:56 -0500

    Added laptop's syncthing id

--- home/syncthing.nix
+++ home/syncthing.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  # ToDo: Find way to set which folder should be synced to "this" device,
+  # as this sets all of them to be made, just who to sync it with.
   home.packages = [ pkgs.syncthingtray ];
   services.syncthing = {
     enable = true;
@@ -16,6 +18,7 @@
           "X201E"
           "T54"
           "Pseduo Aurora"
+          "Y370"
         ];
       };
       "Mods" = {
@@ -34,6 +37,7 @@
       "X201E".id = "5W7WFI3-R26ENXL-QL6IOJE-TKMWLMH-5R6CAHN-C2HXOF7-N7TMTGG-VFIUNAW";
       "T54".id = "2IUACYR-PTA7BZT-YR6CLOR-IKLPVJY-LXZQ337-S5HJJW2-QG53HQA-TFM2XQL";
       "Pseduo Aurora".id = "LTOHBWQ-DNC5T45-JJGORGT-QYSXEOI-USCNKHT-K4EMF7T-TQ52LJ5-SYACMAJ";
+      "Y370".id = "F3I6AYM-MC7JXOD-2INCSSN-7E3AWUG-76ZS4UW-TA6FYU5-TVX2K5Y-SVO73Q5";
     };
   };
 }
