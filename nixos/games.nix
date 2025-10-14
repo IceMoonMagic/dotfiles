91f738f2f4deb58ce442ba02265c3a04f3be9ac6
IceMoonMagic <icemoonmagic@gmail.com>
2025-10-13 19:26:43 -0500

    Added dolphin-emu and custom joycond
    Custom alls motion to work without root,
    only kinda understand why any of this is the way it is...

--- nixos/games.nix
+++ nixos/games.nix
@@ -8,6 +8,25 @@
       extraEnv.PROTON_ENABLE_WAYLAND = 1;
     };
   };
+  services.joycond.enable = true;
+  services.joycond.package = pkgs.joycond.overrideAttrs (old: {
+    # Without: I couldn't use the joycons' motion as non-root
+    # With: Cemuhook doesn't combine them, but motion is accessable
+    version = "2025-04-12";
+    src = pkgs.fetchFromGitHub {
+      owner = "DanielOgorchock";
+      repo = "joycond";
+      rev = "39d5728d41b70840342ddc116a59125b337fbde2";
+      sha256 = "sha256-VT433rrgZ6ltdXLQRjtjRy7rhMl1g9dan9SRqlsCPTk=";
+    };
+    installPhase = old.installPhase + ''
+      substituteInPlace $out/etc/udev/rules.d/72-joycond.rules --replace \
+        'TAG-="uaccess"' 'TAG+="uaccess"'
+
+      rm $out/etc/udev/rules.d/89-joycond.rules
+    '';
+  });
+  # programs.joycond-cemuhook.enable = true;
   environment.systemPackages = with pkgs; [
     steamcmd
     heroic
@@ -17,5 +36,6 @@
     protonup-qt
     satisfactorymodmanager
     r2modman
+    dolphin-emu
   ];
 }
