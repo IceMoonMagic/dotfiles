99b06d7bd2527e805d4dd9c9384a7e90deb3a227
IceMoonMagic <icemoonmagic@gmail.com>
2025-12-05 22:54:29 -0600

    Updated zed config

--- home/development/editors/zed-editor.nix
+++ home/development/editors/zed-editor.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   programs.zed-editor = {
@@ -10,11 +10,31 @@
       "html"
       "nix"
     ];
-    extraPackages = [ pkgs.nixd ];
+    extraPackages = with pkgs; [
+      nil
+      nixd
+    ];
     userSettings = {
       autosave = "on_focus_change";
       disable_ai = true;
       buffer_font_size = 16;
+      # https://github.com/zed-industries/zed/issues/18982
+      # buffer_font_family = "NotoMono Nerd Font";
+      buffer_font_family = "Noto Sans Mono";
+      drag_and_drop_selection.delay = 0;
+      terminal.cursor_shape = "bar";
+      collaboration_panel.button = false;
+      search.button = false;
+      title_bar.show_sign_in = false;
     };
+    userKeymaps = [
+      {
+        context = "Terminal";
+        bindings.ctrl-s = [
+          "terminal::SendKeystroke"
+          "ctrl-s"
+        ];
+      }
+    ];
   };
 }
