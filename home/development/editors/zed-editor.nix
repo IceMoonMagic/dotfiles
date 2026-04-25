776c0e0b06cee0ab40d769e7be3cf1f822a5bdce
IceMoonMagic <icemoonmagic@gmail.com>
2026-04-25 17:39:11 -0500

    Added workaround to get zed to see the nerd font

--- home/development/editors/zed-editor.nix
+++ home/development/editors/zed-editor.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   programs.zed-editor = {
@@ -21,11 +26,10 @@
       autosave = "on_focus_change";
       disable_ai = true;
       buffer_font_size = 16;
-      # https://github.com/zed-industries/zed/issues/18982
-      # buffer_font_family = "NotoSansM Nerd Font";
       buffer_font_family = "Noto Sans Mono";
       drag_and_drop_selection.delay = 0;
       terminal.cursor_shape = "bar";
+      terminal.font_family = "NotoSansM Nerd Font";
       collaboration_panel.button = false;
       search.button = false;
       title_bar.show_sign_in = false;
@@ -44,4 +48,25 @@
       }
     ];
   };
+
+  # https://github.com/zed-industries/zed/issues/18982
+  home.activation.zed-editor =
+    let
+      # Same as /etc/nixos/system/modules/defaults/packages.nix
+      font-pkg = (
+        pkgs.nerd-fonts.noto.overrideAttrs {
+          preInstall = "find -not -name NotoSansMNerdFont-Regular.ttf -delete";
+        }
+      );
+    in
+    lib.mkIf config.programs.zed-editor.enable (
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        src='${font-pkg}/share/'
+        dataHome='${config.xdg.dataHome}/'
+        path='fonts/truetype/NerdFonts/Noto/NotoSansMNerdFont-Regular.ttf'
+        if [ ! -e "$dataHome/$path" ]; then
+          run install $VERBOSE_ARG -D "$src/$path" "$dataHome/$path"
+        fi
+      ''
+    );
 }
