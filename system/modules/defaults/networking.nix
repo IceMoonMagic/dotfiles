fca939ee2ff4c8c4fa8675eab94c9fc7caea4bf9
IceMoonMagic <icemoonmagic@gmail.com>
2026-06-07 03:26:54 -0500

    Changed nameservers and re-enabled netbird
    Quad9 seems to sometimes just take ~8 seconds to resolve.
    I originally disable netbird due to the same thing,
    but netbird was set to use Quad9.
    Both system and netbird are now set to use cloudflare by default,
    but can will use Quad9 as a fallback.
    Also added the IPv6 address for the nameservers.

--- system/modules/defaults/networking.nix
+++ system/modules/defaults/networking.nix
@@ -3,12 +3,16 @@
   networking.networkmanager.enable = lib.mkDefault true;
   networking.networkmanager.dns = "none";
   networking.nameservers = [
-    # Quad9
-    "9.9.9.9"
-    "149.112.112.112"
     # Cloudflare
     "1.1.1.1"
+    "2606:4700:4700::1111"
     "1.0.0.1"
+    "2606:4700:4700::1001"
+    # Quad9
+    "9.9.9.9"
+    "2620:fe::fe"
+    "149.112.112.112"
+    "2620:fe::9"
   ];
-  #   services.netbird.enable = true;
+  services.netbird.enable = true;
 }
